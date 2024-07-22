fa9c943540a241529a5f1d3a281657d5f7c3ace4
Roger Granados <rogergranados217@gmail.com>
2024-07-21 19:01:03 -0600

    ejercicio4

---
+++ ejercicio 4/ejercicio 4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingresa un número para determinar si es o no un número primo: ");
+            int numero = int.Parse(Console.ReadLine());
+            if (numero < 2)
+            {
+                Console.WriteLine($"{numero} no es un número primo. ");
+            }
+            else
+            {
+                bool esprimo = true;
+                for (int i = 2; i <= Math.Sqrt(numero); i++)
+                {
+                    if (numero % i == 0)
+                    { esprimo = false; break; }
+                }
+                if (esprimo)
+                { Console.WriteLine($"{numero} es un número primo. ");
+                }
+                else
+                {
+                    Console.WriteLine($"{numero} no es un número primo");
+                }
+
+            }
+            Console.WriteLine("Presione cualquier tecla para cerrar el programa...");
+            Console.ReadKey();
+        }
+    }
+}
