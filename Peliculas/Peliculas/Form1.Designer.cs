73e40a244aacbdd3cc215de4ce80a16a612749f1
Roger Granados <rogergranados217@gmail.com>
2024-07-27 14:58:03 -0600

    proyecto unidad 2

---
+++ Peliculas/Peliculas/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace Peliculas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Label idLabel;
+            System.Windows.Forms.Label tituloLabel;
+            System.Windows.Forms.Label autorLabel;
+            System.Windows.Forms.Label sinopsisLabel;
+            System.Windows.Forms.Label duracionLabel;
+            System.Windows.Forms.Label clasificacionLabel;
+            this.database1DataSet = new Peliculas.Database1DataSet();
+            this.peliculasBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.peliculasTableAdapter = new Peliculas.Database1DataSetTableAdapters.PeliculasTableAdapter();
+            this.tableAdapterManager = new Peliculas.Database1DataSetTableAdapters.TableAdapterManager();
+            this.idLabel1 = new System.Windows.Forms.Label();
+            this.tituloTextBox = new System.Windows.Forms.TextBox();
+            this.autorTextBox = new System.Windows.Forms.TextBox();
+            this.sinopsisTextBox = new System.Windows.Forms.TextBox();
+            this.duracionTextBox = new System.Windows.Forms.TextBox();
+            this.clasificacionTextBox = new System.Windows.Forms.TextBox();
+            idLabel = new System.Windows.Forms.Label();
+            tituloLabel = new System.Windows.Forms.Label();
+            autorLabel = new System.Windows.Forms.Label();
+            sinopsisLabel = new System.Windows.Forms.Label();
+            duracionLabel = new System.Windows.Forms.Label();
+            clasificacionLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.peliculasBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // idLabel
+            // 
+            idLabel.AutoSize = true;
+            idLabel.Location = new System.Drawing.Point(28, 34);
+            idLabel.Name = "idLabel";
+            idLabel.Size = new System.Drawing.Size(21, 16);
+            idLabel.TabIndex = 0;
+            idLabel.Text = "Id:";
+            // 
+            // tituloLabel
+            // 
+            tituloLabel.AutoSize = true;
+            tituloLabel.Location = new System.Drawing.Point(28, 82);
+            tituloLabel.Name = "tituloLabel";
+            tituloLabel.Size = new System.Drawing.Size(58, 16);
+            tituloLabel.TabIndex = 2;
+            tituloLabel.Text = "TITULO:";
+            // 
+            // autorLabel
+            // 
+            autorLabel.AutoSize = true;
+            autorLabel.Location = new System.Drawing.Point(28, 135);
+            autorLabel.Name = "autorLabel";
+            autorLabel.Size = new System.Drawing.Size(58, 16);
+            autorLabel.TabIndex = 4;
+            autorLabel.Text = "AUTOR:";
+            // 
+            // sinopsisLabel
+            // 
+            sinopsisLabel.AutoSize = true;
+            sinopsisLabel.Location = new System.Drawing.Point(28, 200);
+            sinopsisLabel.Name = "sinopsisLabel";
+            sinopsisLabel.Size = new System.Drawing.Size(72, 16);
+            sinopsisLabel.TabIndex = 6;
+            sinopsisLabel.Text = "SINOPSIS:";
+            // 
+            // duracionLabel
+            // 
+            duracionLabel.AutoSize = true;
+            duracionLabel.Location = new System.Drawing.Point(28, 273);
+            duracionLabel.Name = "duracionLabel";
+            duracionLabel.Size = new System.Drawing.Size(81, 16);
+            duracionLabel.TabIndex = 8;
+            duracionLabel.Text = "DURACION:";
+            // 
+            // clasificacionLabel
+            // 
+            clasificacionLabel.AutoSize = true;
+            clasificacionLabel.Location = new System.Drawing.Point(28, 320);
+            clasificacionLabel.Name = "clasificacionLabel";
+            clasificacionLabel.Size = new System.Drawing.Size(108, 16);
+            clasificacionLabel.TabIndex = 10;
+            clasificacionLabel.Text = "CLASIFICACION:";
+            // 
+            // database1DataSet
+            // 
+            this.database1DataSet.DataSetName = "Database1DataSet";
+            this.database1DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // peliculasBindingSource
+            // 
+            this.peliculasBindingSource.DataMember = "Peliculas";
+            this.peliculasBindingSource.DataSource = this.database1DataSet;
+            // 
+            // peliculasTableAdapter
+            // 
+            this.peliculasTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.PeliculasTableAdapter = this.peliculasTableAdapter;
+            this.tableAdapterManager.UpdateOrder = Peliculas.Database1DataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // idLabel1
+            // 
+            this.idLabel1.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.peliculasBindingSource, "Id", true));
+            this.idLabel1.Location = new System.Drawing.Point(136, 37);
+            this.idLabel1.Name = "idLabel1";
+            this.idLabel1.Size = new System.Drawing.Size(100, 23);
+            this.idLabel1.TabIndex = 1;
+            this.idLabel1.Text = "label1";
+            // 
+            // tituloTextBox
+            // 
+            this.tituloTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.peliculasBindingSource, "Titulo", true));
+            this.tituloTextBox.Location = new System.Drawing.Point(139, 82);
+            this.tituloTextBox.Name = "tituloTextBox";
+            this.tituloTextBox.Size = new System.Drawing.Size(180, 22);
+            this.tituloTextBox.TabIndex = 3;
+            // 
+            // autorTextBox
+            // 
+            this.autorTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.peliculasBindingSource, "Autor", true));
+            this.autorTextBox.Location = new System.Drawing.Point(139, 135);
+            this.autorTextBox.Name = "autorTextBox";
+            this.autorTextBox.Size = new System.Drawing.Size(126, 22);
+            this.autorTextBox.TabIndex = 5;
+            // 
+            // sinopsisTextBox
+            // 
+            this.sinopsisTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.peliculasBindingSource, "Sinopsis", true));
+            this.sinopsisTextBox.Location = new System.Drawing.Point(139, 182);
+            this.sinopsisTextBox.Multiline = true;
+            this.sinopsisTextBox.Name = "sinopsisTextBox";
+            this.sinopsisTextBox.Size = new System.Drawing.Size(296, 63);
+            this.sinopsisTextBox.TabIndex = 7;
+            // 
+            // duracionTextBox
+            // 
+            this.duracionTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.peliculasBindingSource, "Duracion", true));
+            this.duracionTextBox.Location = new System.Drawing.Point(139, 267);
+            this.duracionTextBox.Name = "duracionTextBox";
+            this.duracionTextBox.Size = new System.Drawing.Size(114, 22);
+            this.duracionTextBox.TabIndex = 9;
+            // 
+            // clasificacionTextBox
+            // 
+            this.clasificacionTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.peliculasBindingSource, "Clasificacion", true));
+            this.clasificacionTextBox.Location = new System.Drawing.Point(153, 314);
+            this.clasificacionTextBox.Name = "clasificacionTextBox";
+            this.clasificacionTextBox.Size = new System.Drawing.Size(100, 22);
+            this.clasificacionTextBox.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(idLabel);
+            this.Controls.Add(this.idLabel1);
+            this.Controls.Add(tituloLabel);
+            this.Controls.Add(this.tituloTextBox);
+            this.Controls.Add(autorLabel);
+            this.Controls.Add(this.autorTextBox);
+            this.Controls.Add(sinopsisLabel);
+            this.Controls.Add(this.sinopsisTextBox);
+            this.Controls.Add(duracionLabel);
+            this.Controls.Add(this.duracionTextBox);
+            this.Controls.Add(clasificacionLabel);
+            this.Controls.Add(this.clasificacionTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.peliculasBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Database1DataSet database1DataSet;
+        private System.Windows.Forms.BindingSource peliculasBindingSource;
+        private Database1DataSetTableAdapters.PeliculasTableAdapter peliculasTableAdapter;
+        private Database1DataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.Label idLabel1;
+        private System.Windows.Forms.TextBox tituloTextBox;
+        private System.Windows.Forms.TextBox autorTextBox;
+        private System.Windows.Forms.TextBox sinopsisTextBox;
+        private System.Windows.Forms.TextBox duracionTextBox;
+        private System.Windows.Forms.TextBox clasificacionTextBox;
+    }
+}
+
---
+++ Peliculas/Peliculas/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Peliculas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void peliculasBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.peliculasBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.database1DataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'database1DataSet.Peliculas' Puede moverla o quitarla según sea necesario.
+            this.peliculasTableAdapter.Fill(this.database1DataSet.Peliculas);
+
+        }
+
+    }
+}
